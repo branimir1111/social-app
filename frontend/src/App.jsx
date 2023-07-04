c4a503ff61be6c4b7b8c120eb6601c57065f9555
Branimir Djordjevic <branimir1111@gmail.com>
2023-07-04 17:51:57 +0200

    fixed pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LandingPage from "../pages/LandingPage";
-import StartLayout from "../pages/StartLayout";
-import ErrorPage from "../pages/ErrorPage";
-import RegisterPage from "../pages/RegisterPage";
-import LoginPage from "../pages/LoginPage";
+import LandingPage from "./pages/LandingPage";
+import StartLayout from "./pages/StartLayout";
+import ErrorPage from "./pages/ErrorPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 const router = createBrowserRouter([
   {
