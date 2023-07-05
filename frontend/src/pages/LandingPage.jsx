73a59194e63b9d13e8b5775123be9b2a557ef775
Branimir Djordjevic <branimir1111@gmail.com>
2023-07-05 20:37:54 +0200

    Landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
   return (
-    <div className="w-screen h-screen grid place-content-center circle-blue">
-      <div className="bg-emerald-500 w-52 h-52 rounded-full shadow-2xl grid place-content-center">
-        <div className="bg-teal-200 w-32 h-32 rounded-full shadow-2xl grid place-content-center">
-          <div className="bg-red-500 w-16 h-16 rounded-full shadow-2xl grid place-content-center">
-            <h1 className="text-center">
-              Land
-              <br />
-              Page
-            </h1>
-          </div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="bg-[#333333] w-[600px] h-3/6 rounded-[5px] grid justify-center items-end py-8 max-sm:w-11/12  max-sm:h-4/6">
+        <div className="text-[3rem] mx-auto">🕸️</div>
+        <h1 className="text-[3rem] text-white text-center">
+          {" "}
+          Spider <span className="text-[#2CB1BC]">Web</span> App
+        </h1>
+        <div className=" mx-auto w-5/6 flex justify-center gap-4 max-sm:flex-col max-sm:items-center">
+          <Link to="register" className=" max-sm:w-full">
+            <button
+              type="button"
+              className=" max-sm:w-full bg-[#2CB1BC] p-[8px] rounded-[5px] text-lg hover:bg-[#0E7C86] text-white transition-all duration-300"
+            >
+              Register
+            </button>
+          </Link>
+          <Link to="login" className=" max-sm:w-full">
+            <button
+              type="button"
+              className=" max-sm:w-full bg-[#2CB1BC] p-[8px] rounded-[5px] text-lg hover:bg-[#0E7C86] text-white transition-all duration-300"
+            >
+              Login / DemoUser
+            </button>
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/pages/StartLayout.jsx
+++ frontend/src/pages/StartLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const StartLayout = () => {
   return (
-    <main>
+    <main className="w-screen h-screen flex place-content-center bg-slate-200 font-fontInter">
       <Outlet />
     </main>
   );
