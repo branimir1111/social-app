dd2994f7b7660371d45d6a1fe82965b36caa7662
Branimir Djordjevic <branimir1111@gmail.com>
2023-07-08 23:49:56 +0200

    home page 1

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
 const HomePage = () => {
+  const articles = Array.from({ length: 9 }, (_, index) => index + 1);
   return (
-    <main className="w-[1280px] flex justify-center px-4 bg-orange-200">
-      <h1>This is home page.</h1>
+    <main className="min-h-screen w-11/12 flex justify-center p-4 bg-orange-200">
+      <div className="w-full grid gap-4">
+        {articles.map((article, index) => {
+          return (
+            <article
+              key={index}
+              className="grid h-[250px] bg-slate-300 place-content-center uppercase rounded-[5px]"
+            >
+              <h1>article {article}</h1>
+            </article>
+          );
+        })}
+      </div>
     </main>
   );
 };
--- frontend/src/pages/SharedLayout.jsx
+++ frontend/src/pages/SharedLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const SharedLayout = () => {
   return (
-    <main className="w-screen h-screen flex flex-col items-center bg-[#333333]">
+    <main className="min-w-screen min-h-content flex flex-col items-center bg-[#333333]">
       <Navbar />
       <Outlet />
     </main>
