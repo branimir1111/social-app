5e9dc30c8cccff8e5d497413345fe98987f07a9c
Branimir Djordjevic <branimir1111@gmail.com>
2023-07-21 10:48:13 +0200

    fixing logo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <div className="w-max flex">
-      <Link to="/main/home" className="text-3xl max-md:text-2xl text-white">
+      <Link to="/main" className="text-3xl max-md:text-2xl text-white">
         🕸️ <span className="max-md:text-xl"></span> Spider{" "}
         <span className="text-violet-500">Web</span>
       </Link>
